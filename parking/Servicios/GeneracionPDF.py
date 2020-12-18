70f3ba70bd586cf3170df97ef01c8fff50345bd2
christian payo <christianpayo32@gmail.com>
2020-12-18 10:33:00 +0100

    Establecidas medidad del ticket

--- parking/Servicios/GeneracionPDF.py
+++ parking/Servicios/GeneracionPDF.py
@@ -2,12 +2,14 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 
 def crearPDF(ticket):
-    w, h = A4
-    c = canvas.Canvas("PDF/ticket"+ticket.vehiculo.matricula+".pdf", pagesize=A4)
-    c.drawImage("img/icono.png",260,h-90,width=50, height=50)
-    c.drawString(240,h-110, "Parking salesianos")
-    c.drawString(230,h-125,"Calle Conde de Bustillo")
-    text = c.beginText(30, h - 190)
+    c = canvas.Canvas("PDF/ticket"+ticket.vehiculo.matricula+".pdf")
+    c.setPageSize((300,300))
+    c.drawImage("img/icono.png",125,240,width=50, height=50)
+    c.setFont("Times-Roman", 15)
+    c.drawString(110,220, "Parking salesianos")
+    c.drawString(95,205,"Calle Conde de Bustillo")
+    c.drawString(30,195,"---------------------------------------------------")
+    text = c.beginText(50, 180)
     text.setFont("Times-Roman", 12)
     text.textLine("PIN asociado : "+str(ticket.pin))
     text.textLine(f"Fecha de entrada :{ticket.fechaEntrada.year}/{ticket.fechaEntrada.month}/{ticket.fechaEntrada.day} {ticket.fechaEntrada.hour}:{ticket.fechaEntrada.minute}:{ticket.fechaEntrada.second}")
@@ -15,9 +17,13 @@ def crearPDF(ticket):
     text.textLine("Matricula :"+ticket.vehiculo.matricula)
     c.drawText(text)
 
-    tarifas=c.beginText(30,h-260)
+    tarifas=c.beginText(110,120)
     tarifas.setFont("Times-Roman",9)
-    tarifas.textLine(f"El coste por minuto es de {ticket.plaza.coste_minimo} € para convertirse en abonado hable con el administrador")
+    tarifas.textLine(f"El coste por minuto es de {ticket.plaza.coste_minimo} €")
     c.drawText(tarifas)
+    c.setFont("Times-Roman",15)
+    c.drawString(30,110,"---------------------------------------------------")
+    c.setFont("Times-Roman",12)
+    c.drawString(50,100,"Gracias por contar con nuestros servicios")
     c.showPage()
     c.save()
